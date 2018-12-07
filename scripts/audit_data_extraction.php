259995b547b68ca2a014895488efa2defbb0d58a
Guilherme <guilherme.muller.m@gmail.com>
2018-12-07 06:11:34 -0200

    Dados de Host

--- scripts/audit_data_extraction.php
+++ scripts/audit_data_extraction.php
@@ -1,15 +1,20 @@
 <?php
 
 include "utils.php";
+require "../vendor/autoload.php";
 
-const GET_AUDIT_FROM_API = false;
+use Nmap\Nmap;
 
-//Caso seja necessário, coleta os dados de auditoria e joga num json
+const GET_AUDIT_FROM_API = true;
+const GET_HOST_SYSTEM_DATA_FROM_API = true;
+
+$falseUrlData = [];
+
+//Coleta os dados de auditoria da API externa ou do JSON previamente preenchido
 if(GET_AUDIT_FROM_API){
     echo "#### Getting audit data from external API ####" . PHP_EOL;
     $falseUrls = getFalseUrls();
     
-    $falseUrlData = [];
     $ipdataAccessKey = file_get_contents("ipdataAccessKey.txt"); //Outra opção é usar o ipinfo.io
     echo "# Getted API access key: $ipdataAccessKey" . PHP_EOL;
     foreach($falseUrls as $url){
@@ -34,24 +39,70 @@ if(GET_AUDIT_FROM_API){
         //Data Mapping 
         $desiredData = (object)[
             "ip" => $response->ip,
-            "isEU" => $response->is_eu,
-            "continent" => $response->continent_name,
-            "country" => $response->country_name,
-            "city" => $response->city,
-            "region" => $response->region,
-            "lat" => $response->latitude,
-            "long" => $response->longitude,
-            "asn" => $response->asn,
-            "organisation" => $response->organisation,
-            "threatData" => $response->threat
+            "url" => $url,
+            "hostData" => (object)[
+                "location" => (object) [
+                    "isEU" => $response->is_eu,
+                    "continent" => $response->continent_name,
+                    "country" => $response->country_name,
+                    "city" => $response->city,
+                    "region" => $response->region,
+                    "lat" => $response->latitude,
+                    "long" => $response->longitude,
+                ],
+                "asn" => $response->asn,
+                "organisation" => $response->organisation,
+                "threatData" => $response->threat
+            ],
         ];
     
         $falseUrlData[] = $desiredData;
     }
     
     //Creates a JSON file to save the results (for now);
-    file_put_contents("../datasets/ipdataoutput.json", json_encode($falseUrlData));
+    persistDataOnFile($falseUrlData, "dataOutput.json");
 } else {
-    
+    echo "#### Getting audit data cached JSON file ####" . PHP_EOL;
+    $falseUrlData = json_decode(file_get_contents("../datasets/ipdataoutput.json"));
 }
+
+//Coleta dados sobre o system dos hosts e atualiza o JSON de output gerado na etapa anterior
+if(GET_HOST_SYSTEM_DATA_FROM_API){
+    echo "#### Getting hosts system information from external API ####" . PHP_EOL;
+
+    $nmap = new Nmap();
+    $nmap
+        ->enableOsDetection(true)
+        ->enableServiceInfo(true);
+
+    foreach ($falseUrlData as &$data) {
+        try{
+            $url = $data->url;
+            $hostIp = $data->ip;
+
+            echo "# Getting data from $url" . PHP_EOL;
+            
+            $tempOutputFilePath = $nmap->scan(['scanme.nmap.org']);
+            $outputXMLString = file_get_contents($tempOutputFilePath);
+            $outputXML = simplexml_load_string($outputXMLString);
+            $outputAsJSON = json_encode($outputXML);
+            $outputAsObject = json_decode($outputAsJSON);
+
+            $clearedResult = clearNmapResult($outputAsObject);
+            
+            $data->hostData->hostNames = $clearedResult->host->hostNames;
+            $data->hostData->ports = $clearedResult->host->ports;
+            $data->OSData = $clearedResult->OS;
+        } catch(\Exception $e){
+            continue;
+        }
+        
+        // var_dump($outputAsObject);
+        // exit;
+    }
+
+    persistDataOnFile($falseUrlData, "dataOutput.json");
+}
+
+
 
--- scripts/utils.php
+++ scripts/utils.php
@@ -24,4 +24,93 @@
 
         return $uniqueUrls;
     }
+
+    function clearNmapResult($nmapResult){
+        $clearedResult = (object)[
+            "host" => (object)[],
+            "OS" => (object)[]
+        ];
+
+        //Pegando dados do host
+        //Se o nmap pegou dados de hostname:
+        if(isset($nmapResult->host, $nmapResult->host->hostnames, $nmapResult->host->hostnames->hostname)){
+            $clearedResult->host->hostNames = [];
+            foreach($nmapResult->host->hostnames->hostname as $hostname){
+                $clearedResult->host->hostNames[] = (object)[
+                    "name" => $hostname->{"@attributes"}->name,
+                    "type" => $hostname->{"@attributes"}->type
+                ];
+            }
+        }
+
+        //Se o nmap pegou dados de portas:
+        if(isset($nmapResult->host, $nmapResult->host->ports, $nmapResult->host->ports->port)){
+            $clearedResult->host->ports = [];
+
+            foreach ($nmapResult->host->ports->port as $port) {
+                $clearedPort = (object)[];
+                if(isset($port->{"@attributes"})){
+                    $clearedPort->protocol = $port->{"@attributes"}->protocol;
+                    $clearedPort->id = $port->{"@attributes"}->portid;
+                }
+                if(isset($port->state)){
+                    $clearedPort->state = $port->state->{"@attributes"}->state;
+                    $clearedPort->stateReson = $port->state->{"@attributes"}->reason;
+                }
+                if(isset($port->service)){
+                    $clearedPort->service = (object)[];
+                    $clearedPort->service->name = $port->service->{"@attributes"}->name;
+                    $clearedPort->service->product = $port->service->{"@attributes"}->product;
+                    $clearedPort->service->version = $port->service->{"@attributes"}->version;
+                    $clearedPort->service->extraInfo = $port->service->{"@attributes"}->extrainfo;
+                    $clearedPort->service->OSType = $port->service->{"@attributes"}->ostype;
+                    $clearedPort->service->method = $port->service->{"@attributes"}->method;
+                    $clearedPort->service->conf = $port->service->{"@attributes"}->conf;
+                }
+
+                $clearedResult->host->ports[] = $clearedPort;
+            }
+        }
+
+        //Se o nmap pegou resultados de OS pt1
+        if(isset($nmapResult->host, $nmapResult->host->os)){
+            if(isset($nmapResult->host->os->portused)){
+                $clearedResult->OS->usedPorts = [];
+
+                foreach($nmapResult->host->os->portused as $usedPort){
+                    $clearedResult->OS->usedPorts[] = (object)[
+                        "state" => $usedPort->{"@attributes"}->state,
+                        "protocol" => $usedPort->{"@attributes"}->proto,
+                        "id" => $usedPort->{"@attributes"}->id
+                    ];
+                }
+            }
+
+            if(isset($nmapResult->host->os->osmatch)){
+                if(is_array($nmapResult->host->os->osmatch) && count($nmapResult->host->os->osmatch) > 0){
+                    $mostProbableMatch = $nmapResult->host->os->osmatch[0];
+
+                    $clearedResult->OS->OSGuess = (object)[];
+                    $clearedResult->OS->OSGuess->name = $mostProbableMatch->{"@attributes"}->name;
+                    $clearedResult->OS->OSGuess->accuracy = $mostProbableMatch->{"@attributes"}->accuracy;
+                    $clearedResult->OS->OSGuess->class = [];
+                    foreach($mostProbableMatch->osclass as $class){
+                        $clearedResult->OS->OSGuess->class[] = (object)[
+                            "type" => $class->{"@attributes"}->type,
+                            "vendor" => $class->{"@attributes"}->vendor,
+                            "family" => $class->{"@attributes"}->osfamily,
+                            "generation" => $class->{"@attributes"}->osgen,
+                            "accuracy" => $class->{"@attributes"}->accuracy,
+                        ];
+                    }
+                }
+            }
+        }
+
+        return $clearedResult;
+    }
+
+    function persistDataOnFile($data, $fileName){
+        file_put_contents("../datasets/$fileName", json_encode($data));
+    }
 ?>
